f84a9ee50109d26c6a7003dfb837fdf2a3219c47
swqa7697 <swqa7697@gmail.com>
2024-11-06 22:15:46 -0800

    Changed "About Me" to activity (removed toast)

--- app/src/main/java/edu/nu/numad24fa_shuzhanxie/AboutMeActivity.java
+++ app/src/main/java/edu/nu/numad24fa_shuzhanxie/AboutMeActivity.java
@@ -1,9 +1,7 @@
 package edu.nu.numad24fa_shuzhanxie;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +9,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutMeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_about_me);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about_me), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setAboutMeInfo();
     }
 
-    public void aboutMeActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
-        startActivity(intent);
-    }
-
-    public void calcActivity(View view) {
-        Intent intent = new Intent(MainActivity.this, CalcActivity.class);
-        startActivity(intent);
+    private void setAboutMeInfo() {
+        TextView info = findViewById(R.id.text_about_me);
+        info.setText("Name: Shuzhan Xie\nEmail: xie.shuz@northeastern.edu");
     }
 }
