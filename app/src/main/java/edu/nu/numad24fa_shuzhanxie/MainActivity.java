84eaf1740cc0158b3784a491f8bd720543ec9ebe
swqa7697 <swqa7697@gmail.com>
2024-11-06 22:16:55 -0800

    Minor fixes

--- app/src/main/java/edu/nu/numad24fa_shuzhanxie/MainActivity.java
+++ app/src/main/java/edu/nu/numad24fa_shuzhanxie/MainActivity.java
@@ -3,7 +3,6 @@ package edu.nu.numad24fa_shuzhanxie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
